0ccf40df97b849ed4a4dd256e715703160ca4bf1
José Carlos Valera <jcarlos@jcarlos-Aspire-5742.(none)>
2015-05-15 23:55:39 +0200

    runner avanzado

--- Documentación/webRunner/runner.js
+++ Documentación/webRunner/runner.js
@@ -14,8 +14,6 @@ var infoNativo = (function(){
 
 window.addEventListener("load",function() {
 
-
-
 var Q = window.Q = Quintus()
         .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
         .setup({ width: 683, height: 384, scaleToFit: true })
@@ -58,6 +56,7 @@ var Q = window.Q = Quintus()
     Q.state.set("coins",infoNativo.coins);
   });
 var SPRITE_BOX = 1;
+var SPRITE_MINA = 2;
 
 Q.gravityY = 2000;
 
@@ -73,6 +72,7 @@ Q.Sprite.extend("Player",{
       releaseJump: false,
       x: 40,
       shield: false,
+      onFire: false,
       y: 610,
       scale: 0.8,
       standingPoints: [ [ -16, 44], [ -23, 35 ], [-23,-48], [23,-48], [23, 35 ], [ 16, 44 ]],
@@ -82,6 +82,7 @@ Q.Sprite.extend("Player",{
       stopped: false,
       dead: false,
       distance: 0,
+      nivelAgachado: 0
     });
 
     this.p.points = this.p.standingPoints;
@@ -91,6 +92,13 @@ Q.Sprite.extend("Player",{
 
   dead: function(){
     this.p.dead = true;
+  },
+
+  setFire: function(activated){
+     if(activated)
+      this.p.onFire = true;
+    else
+      this.p.onFire = false;
   },
 
   stopped: function(){
@@ -130,6 +138,12 @@ Q.Sprite.extend("Player",{
     } else {
       this.p.landed = 0;
     }
+    if(this.p.sprite == "agachado"){
+
+        this.p.nivelAgachado = this.p.y;
+        this.p.y = this.p.nivelAgachado + 25;
+
+    }
 
     if(this.p.doubleJump && this.p.releaseJump){
       if(Q.inputs['up']){
@@ -148,14 +162,19 @@ Q.Sprite.extend("Player",{
     if(this.p.landed) {
       this.p.doubleJump = true;
       this.p.releaseJump = false;
-      if(Q.inputs['down'] && this.p.animation != "eat") {
-        console.log(this);
+      if(Q.inputs['down'] && this.p.y >=610 && this.p.animation != "eat") {
+
+        this.p.sheet = "agachado";
         this.p.sprite = "agachado";
         this.play("agachado");
         //this.size(true);
         this.p.points = this.p.duckingPoints;
       } else {
-        this.p.sprite = "eater";
+        if(this.p.sheet == "agachado"){
+          this.p.sheet = "eater";
+          this.p.sprite = "eater";
+        }
+       // this.p.sprite = "eater";
         if(this.p.animation != "eat")
           this.play("walk");
       }
@@ -176,12 +195,6 @@ Q.Sprite.extend("Player",{
   }
 });
 
-Q.component("defaultEnemy", {
-  added: function(){
-      var entity = this.entity;
-
-  }
-});
 
 Q.Sprite.extend("Mina",{
   init: function() {
@@ -196,12 +209,12 @@ Q.Sprite.extend("Mina",{
       scale: 1.2,
       type: SPRITE_BOX,
       sheet: "mina",
-      vx: 50,
+      vx: - 200,
       vy: 0,
       ay: 0,
       theta: (300 * Math.random() + 200) * (Math.random() < 0.5 ? 1 : -1)
     });
-
+    this.add("animation, tween");
 
     this.on("hit");
   },
@@ -219,7 +232,13 @@ Q.Sprite.extend("Mina",{
   },
   hit: function(collision) {
     var player = collision.obj;
-    if(this.p.type != 0 && player.isA("Player")){
+    if(this.p.type != 0 && player.isA("Player") && (player.p.onFire || player.p.shield)){
+      this.p.type = 0;
+      player.play("eat");
+      var playerVel = player.p.vx;
+      //console.log("caja: x-> " + this.p.x + " y-> " + this.p.y + " |  Pj: x-> " + collision.obj.p.x + " y-> " + collision.obj.p.y);
+      this.animate({y: player.p.y, vx: playerVel + playerVel/1.5, opacity: 0, scale: 0.1}, 1/5, {callback: function(){this.destroy();}});
+    }else if(this.p.type != 0 && player.isA("Player")){
       player.p.vx = 0;
       player.dead();
       Q.stageScene("endGame", 1, { label: "Game Over!" });
@@ -229,6 +248,8 @@ Q.Sprite.extend("Mina",{
 
       this.p.opacity = 0.5;
     }
+    else if(player.isA("BoxFija"))
+      player.destroy();
       this.p.vx = 200;
       this.p.ay = 400;
       this.p.vy = -300;
@@ -251,7 +272,7 @@ Q.Sprite.extend("BoxGood",{
       scale: 0.9,
       type: SPRITE_BOX,
       sheet: "mina",
-      vx: -200 + 200 * Math.random(),
+      vx: -200 + 400 * Math.random(),
       vy: 0,
       ay: 0,
       sensor: true,
@@ -315,7 +336,7 @@ Q.Sprite.extend("BoxFija",{
       player: Q("Player").first(),
       touched: false,
       x: player.p.x + Q.width + 50,
-      y: 573 - (50 * posY),
+      y: 610 - (50 * posY),
       //frame: Math.random() < 0.5 ? 1 : 0,
       scale: 0.8,
       type: SPRITE_BOX,
@@ -327,7 +348,7 @@ Q.Sprite.extend("BoxFija",{
     this.p.x += this.p.w * this.p.scale * posX;
     this.add("animation, tween");
 
-    //this.on("hit", this, "sensor");
+    this.on("hit");
 
     //this.add("animation, tween");
   },
@@ -343,32 +364,19 @@ Q.Sprite.extend("BoxFija",{
 
   },
 
-  sensor: function(collision) {
-    /*var playerObj = collision.obj;
-    if(!this.p.touched && playerObj.isA("Player")){
-      Q.state.inc("score", 1);
+  hit: function(collision) {
+    var playerObj = collision.obj;
+    if(!this.p.touched && playerObj.isA("Player") && playerObj.p.onFire){
       this.p.touched = true; 
-      playerObj.play("eat");
+      this.p.type = 0;
       var playerVel = playerObj.p.vx;
       //console.log("caja: x-> " + this.p.x + " y-> " + this.p.y + " |  Pj: x-> " + collision.obj.p.x + " y-> " + collision.obj.p.y);
       this.animate({y: playerObj.p.y, vx: playerVel + playerVel/2, opacity: 0, scale: 0.1}, 1/5, {callback: function(){this.destroy();}});
-    }else{
     }
-
-      
-    this.p.type = 0;
-    this.p.collisionMask = Q.SPRITE_NONE;
-    this.p.vx = 200;
-    this.p.ay = 400;
-    this.p.vy = -300;
-    this.p.opacity = 1;
-  */
   }
   
 
 });
-
-
 
 Q.Sprite.extend("Ameba",{
   init: function(level) {
@@ -519,21 +527,163 @@ Q.Sprite.extend("Escudo",{
 
 });
 
+/////////////////////////////////////////////////////////////////////////////
 
-
-//////////////////////////////COIN////////////////////////////////
-
-
-Q.Sprite.extend("Coin",{
+Q.Sprite.extend("FuegoIcon",{
   init: function(tipo) {
 
     var levels = [ 573 , 515 , 509 ];
 
     var player = Q("Player").first();
     this._super({
+
+      player: Q("Player").first(),
       touched: false,
       x: player.p.x + Q.width + 50,
       y: 509,
+      scale: 0.2,
+      type: SPRITE_BOX,
+
+      opacity: 0.8,
+      sheet: "fuego",
+      sprite: "fuego",
+      sensor: true,
+      vx: 50,
+      vy: 0,
+      ay: 0,
+    });
+
+    this.add("animation, tween");
+    this.animateFire();
+    this.on("hit", this, "sensor");
+
+    },
+
+  animateFire: function(){
+    this
+      .animate({scale: 0.4}, 0.7 )
+      .chain({scale: 0.7}, 0.7, {
+        callback: function(){this.animateFire();}});
+  },
+  step: function(dt) {
+
+    this.p.x += this.p.vx * dt;
+
+
+    this.p.vy += this.p.ay * dt;
+    this.p.y += this.p.vy * dt;
+
+
+    if(this.p.y > 800 || this.p.x < this.p.player.p.x - 200) { console.log("BYE");this.destroy(); }
+
+
+  },
+
+  sensor: function(collision) {
+    var playerObj = collision.obj;
+    if(!this.p.touched && playerObj.isA("Player")){
+      playerObj.play("eat");
+      this.p.touched = true;
+      if(!playerObj.p.onFire){
+        this.stage.insert(new Q.Fuego());
+        playerObj.setFire(true);
+      }
+      this.animate({y: this.p.y - 30}, 1/5, {callback: function(){this.destroy();}});
+    }
+  }
+  
+
+});
+///////////////////////////////////////////////////////////////////////////
+
+
+Q.Sprite.extend("Fuego",{
+  init: function(level) {
+
+    var levels = [ 573 , 515 , 509 ];
+
+    var player = Q("Player").first();
+    this._super({
+      player: Q("Player").first(),
+      x: player.p.x,
+      //y: tipo == "doble" ? 515  : 573,
+      y: player.p.y - 20,
+      //frame: Math.random() < 0.5 ? 1 : 0,
+      scale: 1.1,
+      type: 0,
+      opacity: 0.6,
+      sheet: "fuego",
+      sprite: "fuego",
+      timeStart: 0,
+      timeMax: level > 0 ? 5*level : 5,
+      vx: player.p.vx,
+      vy: player.p.vy,
+      ay: player.p.ay,
+    });
+
+    this.add("animation, tween");
+    
+    this.play("fuego");
+
+    this.on("hit", this, "sensor");
+
+    },
+
+  step: function(dt) {
+
+    this.p.timeStart += dt;
+    this.p.x = this.p.player.p.x;
+
+    this.p.vy = this.p.player.p.vy;
+    this.p.y = this.p.player.p.y - 20;
+    
+    if(this.p.timeStart > this.p.timeMax){ 
+      this.p.player.setFire(false); 
+      this.destroy();
+    }
+
+  },
+
+  sensor: function(collision) {
+    /*var playerObj = collision.obj;
+    if(!this.p.touched && playerObj.isA("Player")){
+      Q.state.inc("score", 1);
+      this.p.touched = true; 
+      playerObj.play("eat");
+      var playerVel = playerObj.p.vx;
+      //console.log("caja: x-> " + this.p.x + " y-> " + this.p.y + " |  Pj: x-> " + collision.obj.p.x + " y-> " + collision.obj.p.y);
+      this.animate({y: playerObj.p.y, vx: playerVel + playerVel/2, opacity: 0, scale: 0.1}, 1/5, {callback: function(){this.destroy();}});
+    }else{
+    }
+
+      
+    this.p.type = 0;
+    this.p.collisionMask = Q.SPRITE_NONE;
+    this.p.vx = 200;
+    this.p.ay = 400;
+    this.p.vy = -300;
+    this.p.opacity = 1;
+  */
+  }
+  
+
+});
+
+
+//////////////////////////////COIN////////////////////////////////
+
+
+Q.Sprite.extend("Coin",{
+  init: function(posX, posY) {
+
+    var levels = [ 573 , 515 , 509 ];
+
+    var player = Q("Player").first();
+    this._super({
+      player: Q("Player").first(),
+      touched: false,      
+      x: player.p.x + Q.width + 50,
+      y: 610 - (50 * posY),
       scale: 0.7,
       type: SPRITE_BOX,
       opacity: 1,
@@ -545,6 +695,7 @@ Q.Sprite.extend("Coin",{
       ay: 0,
       theta: (300 * Math.random() + 200) * (Math.random() < 0.5 ? 1 : -1)
     });
+    this.p.x += this.p.w * this.p.scale * posX + 20;
 
     this.add("tween");
     
@@ -564,14 +715,14 @@ Q.Sprite.extend("Coin",{
     this.p.vy += this.p.ay * dt;
     this.p.y += this.p.vy * dt;
     
-    if(this.p.y > 800) { this.destroy(); }
+    if(this.p.y > 800 || this.p.x < this.p.player.p.x - 200) { this.destroy(); }
 
   },
 
   animateCoin: function(){
     this
-      .animate({scale: 0.5}, 1 )
-      .chain({scale: 0.9}, 1, {
+      .animate({scale: 0.4}, 1 )
+      .chain({scale: 0.7}, 1, {
         callback: function(){this.animateCoin();}});
   },
 
@@ -614,6 +765,30 @@ Q.GameObject.extend("BoxThrower",{
     }
   },
 
+  struct1: function(){
+
+          //this.stage.insert(new Q.FuegoIcon());
+          this.stage.insert(new Q.BoxFija(1,0));
+          this.stage.insert(new Q.Coin(2,0));
+          this.stage.insert(new Q.BoxFija(2,1));
+          this.stage.insert(new Q.BoxFija(3,0));
+  },
+  struct2: function(){
+          this.stage.insert(new Q.BoxFija(1,0));
+          this.stage.insert(new Q.BoxFija(2,0));
+          this.stage.insert(new Q.BoxFija(4,2));
+          this.stage.insert(new Q.BoxFija(5,2));
+          this.stage.insert(new Q.BoxFija(6,2));
+          this.stage.insert(new Q.BoxFija(8,0));
+          this.stage.insert(new Q.BoxFija(9,0));
+          this.stage.insert(new Q.Coin(3,0));
+          this.stage.insert(new Q.Coin(4,0));
+          this.stage.insert(new Q.Coin(5,0));
+          this.stage.insert(new Q.Coin(6,0));
+          this.stage.insert(new Q.Coin(7,0));
+          
+  },
+
   update: function(dt) {
     if(this.p.player==null)
       this.p.player=Q("Player").first();
@@ -626,10 +801,7 @@ Q.GameObject.extend("BoxThrower",{
           
         if(Math.floor(Math.random()*2)===1){
           //this.stage.insert(new Q.BoxFija("doble"));
-          this.stage.insert(new Q.Escudo());
-          this.stage.insert(new Q.BoxFija(1,1));
-          this.stage.insert(new Q.BoxFija(2,1));
-          this.stage.insert(new Q.BoxFija(3,1));
+          this.struct2();
           this.p.launch = this.p.launchDelay + this.p.launchRandom * Math.random();
         }else{
           this.stage.insert(new Q.Mina());
@@ -669,8 +841,8 @@ Q.GameObject.extend("BoxThrower",{
     buttonPlay.on("click",function() {
       Q.clearStages();
       Q.stageScene('level1');
-      Q.stageScene("HUD", 1);
-      Q.stageScene("HUDCoins", 2);
+      Q.stageScene("HUD", 2);
+      Q.stageScene("HUDCoins", 3);
     });
     buttonExit.on("click", function(){
       Q.clearStages();
@@ -700,7 +872,8 @@ Q.scene("level1",function(stage) {
 
 });
   
-Q.load("fondo.jpg, snow.png, agachado.png, agachado.json, muerto.png, coin.png, coin.json, escudo.png, escudo.json, ameba.png, ameba.json, cajaRota.png, cajaRota.json, mina.png, mina.json, eater.json, eater.png, player.json, player.png, background-wall.png, crates.png, crates.json, cajaBuena.png, cajaBuena.json", function() {
+Q.load("fuego.json, fuego.png, fondo.jpg, agachado.png, agachado.json,  muerto.png, coin.png, coin.json, escudo.png, escudo.json, ameba.png, ameba.json, cajaRota.png, cajaRota.json, mina.png, mina.json, eater.json, eater.png, player.json, player.png, background-wall.png, crates.png, crates.json, cajaBuena.png, cajaBuena.json", 
+  function() {
     Q.compileSheets("player.png","player.json");
     Q.compileSheets("crates.png","crates.json");
     Q.compileSheets("cajaBuena.png","cajaBuena.json");
@@ -711,6 +884,11 @@ Q.load("fondo.jpg, snow.png, agachado.png, agachado.json, muerto.png, coin.png, 
     Q.compileSheets("escudo.png", "escudo.json");
     Q.compileSheets("coin.png", "coin.json");
     Q.compileSheets("agachado.png", "agachado.json");
+    Q.compileSheets("fuego.png", "fuego.json");
+
+    Q.animations("fuego", {
+      fuego: {frames: [0, 1], rate: 1/4, loop: true }
+    });
 
     Q.animations("eater", {
       walk: { frames: [0,1,2,1], rate: 1/5, flip: false, loop: true },
